810aa760c76e98ada76cc7587c3170c121f4b2da
chenhzjs <chenhzjs@foxmail.com>
2024-11-11 20:46:30 +0800

    finish amazon crwaler & update the environment

---
+++ crawler/src/main.py
@@ -0,0 +1,9 @@
+
+
+if __name__ == "__main__":
+    
+    if len(sys.argv) > 1:
+        keyword = sys.argv[1]
+    else:
+        print("Please provide a keyword as a command line argument (English may be better).")
+        sys.exit(1)
