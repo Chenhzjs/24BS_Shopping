82448c42101651ff6f0922fda8f848a31fbb89fd
chenhzjs <chenhzjs@foxmail.com>
2024-11-12 13:07:52 +0800

    add web dir

---
+++ web/js/script.js
@@ -0,0 +1,8 @@
+// script.js
+
+// Wait for the DOM to be fully loaded
+document.addEventListener('DOMContentLoaded', (event) => {
+    console.log('DOM fully loaded and parsed');
+
+    // Your code here
+});
